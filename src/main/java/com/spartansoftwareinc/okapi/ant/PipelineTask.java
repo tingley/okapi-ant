c66f916589353de4b9ecd3e420c501d75ec1155c
Chase Tingley <chase@spartansoftwareinc.com>
2013-06-23 10:30:22 -0700

    Add sample build.xml and minimal task logic that loads okapi

--- src/main/java/com/spartansoftwareinc/okapi/ant/PipelineTask.java
+++ src/main/java/com/spartansoftwareinc/okapi/ant/PipelineTask.java
@@ -1,5 +1,14 @@
 package com.spartansoftwareinc.okapi.ant;
 
+import java.io.File;
+
+import net.sf.okapi.applications.rainbow.batchconfig.BatchConfiguration;
+import net.sf.okapi.applications.rainbow.pipeline.PipelineWrapper;
+import net.sf.okapi.common.ExecutionContext;
+import net.sf.okapi.common.filters.DefaultFilters;
+import net.sf.okapi.common.filters.FilterConfigurationMapper;
+import net.sf.okapi.common.plugins.PluginsManager;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
@@ -19,26 +28,87 @@ public class PipelineTask extends Task {
 	//    <!-- file information -->
 	// </pipeline>
 	
-	private String bconf;
+	private String bconfPath;
 	
 	public void setBconf(String val) {
-		this.bconf = val;
+		this.bconfPath = val;
 	}
 	
+	// TODO - need to fiddle with shade config to exclude osx/swt stuff?
 	public void execute() {
 		checkConfiguration();
-		Project project = getProject();
 		
 		// Let's start by specifying a pipeline
 		// Alternately, should I take a whole settings file?  That would give me the mappings.
 		// Or, I could just take a bconf.  That would give me the plugins as well.
 
-		log("bconf: " + bconf);
+		log("bconf: " + bconfPath);
+		
+		File bconfFile = getBconfFile(bconfPath);
+		// Should probably create a temp working space, since this needs a lot of file paths
+
+		File baseDir = new File("/tmp/bconf"); // XXX
+		PluginsManager plManager = new PluginsManager();
+		plManager.discover(new File(getConfigDirPath(baseDir)), true);
+		PipelineWrapper pipelineWrapper = preparePipelineWrapper(baseDir, plManager);
+        BatchConfiguration bconf = new BatchConfiguration();
+        bconf.installConfiguration(bconfFile.getAbsolutePath(),
+                getConfigDirPath(baseDir), pipelineWrapper);
+	}
+	
+	// XXX This probably needs to expand the bconf somewhere temporary so that it can install 
+	// the plugins, etc?
+	private PipelineWrapper preparePipelineWrapper(File baseDir, PluginsManager plManager) {
+        // Load local plug-ins                                           
+		//plManager.discover(new File(WorkspaceUtils.getConfigDirPath(projId)), true);
+        
+		// Initialize filter configurations
+        FilterConfigurationMapper fcMapper = new FilterConfigurationMapper();
+        DefaultFilters.setMappings(fcMapper, false, true);
+        //fcMapper.addFromPlugins(plManager);
+        //fcMapper.setCustomConfigurationsDirectory(WorkspaceUtils.getConfigDirPath(projId));
+        fcMapper.updateCustomConfigurations();
+
+        // Load pipeline
+        ExecutionContext context = new ExecutionContext();
+        context.setApplicationName("okapi-ant");
+        context.setIsNoPrompt(true);
+        PipelineWrapper pipelineWrapper = new PipelineWrapper(fcMapper, getConfigDirPath(baseDir),
+                plManager, getInputDirPath(baseDir), getInputDirPath(baseDir),
+                null, context);
+        pipelineWrapper.addFromPlugins(plManager);
+        return pipelineWrapper;
+    }
+	
+	private String getConfigDirPath(File base) {
+		return getPath(new File(base, "config"));
+	}
+	
+	private String getInputDirPath(File base) {
+		return getPath(new File(base, "input"));
+	}
+	
+	private String getPath(File dir) {
+		dir.mkdirs();
+		return dir.getPath();
+	}
+	
+	private File getBconfFile(String path) {
+		File f = new File(path);
+		if (!f.exists() || !f.isFile()) {
+			throw new BuildException("Not a bconf file: " + path);
+		}
+		return f;
 	}
 	
 	private void checkConfiguration() {
-		if (bconf == null) {
+		if (bconfPath == null) {
 			throw new BuildException(BCONF_ATTR + " was not set");
 		}
+	}
+	
+	// XXX remove
+	public static void main(String[] args) {
+		System.out.println("Hello world");
 	}
 }
