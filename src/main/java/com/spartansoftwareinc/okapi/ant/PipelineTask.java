92d38fa836acf2f9ea8a171ca8627ac35b3f4811
Chase Tingley <chase@spartansoftwareinc.com>
2015-02-16 23:16:33 -0800

    Rewrite pipeline task so it actually works (as exec-pipeline)
        This allows you to execute an arbitrary pln file, specifying src/tgt
        locales and encodings, source files, and filter mappings for all
        source files (required, due to some ugliness involving limitations
        of Okapi's FilterConfigurationMapper).  eg:
    
        <okapi:exec-pipeline pipeline="pipeline.pln"
                    srcLang="en-US" tgtLang="fr-FR"
                    srcEncoding="UTF-8" tgtEncoding="UTF-8">
            <filterMapping extension=".html" filterConfig="okf_html" />
            <fileset dir="files" includes="**/*.html" />
        </okapi:exec-pipeline>

--- src/main/java/com/spartansoftwareinc/okapi/ant/PipelineTask.java
+++ src/main/java/com/spartansoftwareinc/okapi/ant/PipelineTask.java
@@ -1,110 +1,166 @@
 package com.spartansoftwareinc.okapi.ant;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-import net.sf.okapi.applications.rainbow.batchconfig.BatchConfiguration;
+import net.sf.okapi.applications.rainbow.Project;
+import net.sf.okapi.applications.rainbow.lib.LanguageManager;
 import net.sf.okapi.applications.rainbow.pipeline.PipelineWrapper;
-import net.sf.okapi.common.filters.DefaultFilters;
+import net.sf.okapi.common.ExecutionContext;
+import net.sf.okapi.common.LocaleId;
 import net.sf.okapi.common.filters.FilterConfigurationMapper;
 import net.sf.okapi.common.plugins.PluginsManager;
 
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
 
-public class PipelineTask extends Task {
-	
-	public static final String BCONF_ATTR = "bconf";
+public class PipelineTask extends BasePipelineTask {
+	/**
+	 * <okapi:exec-pipeline pipeline="foo.pln" srcLang="en" tgtLang="fr"
+	 * 			srcEncoding="UTF-8" tgtEncoding="UTF-8" filterConfigDir="filterConfigs">
+	 * 		<filterMapping extension=".xml" filterConfig="okf_xmlstream@myConfig" />
+	 * 		<fileset dir="files" includes"..." />
+	 * 		<!-- How to handle plugins? I will need to figure it out. 
+	 *           I think I need a <plugins> element with embedded filesets. -->
+	 * </okapi:exec-pipeline>
+	 *
+	 * Things to do: - Handle plugins - Specify output - I might need to specify
+	 * the baseDir somehow?
+	 */
 
-	// Sample rainbow command line:
-	// rainbow -np -pln pseudo.pln -sl en -se utf-8 -tl fr -te utf-8 simple.dfxp.xml -fc okf_dfxpstream -o out
-	//
-	// More reference: 
-	//   http://www.opentag.com/okapi/wiki/index.php?title=Rainbow_-_Command_Line
+	private String srcLang, tgtLang, srcEncoding, tgtEncoding;
+	private String plnPath;
+	private String filterConfigPath;
+	private List<FileSet> filesets = new ArrayList<FileSet>();
+	private List<FilterMapping> filterMappings = new ArrayList<FilterMapping>();
 
-	// What's the structure?
-	// <pipeline bconf="...">
-	//    <!-- file information -->
-	// </pipeline>
-	
-	private String bconfPath;
-	
-	public void setBconf(String val) {
-		this.bconfPath = val;
+	public void setSrcLang(String value) {
+		this.srcLang = value;
 	}
-	
-	// TODO - need to fiddle with shade config to exclude osx/swt stuff?
-	public void execute() {
-		checkConfiguration();
-		
-		// Let's start by specifying a pipeline
-		// Alternately, should I take a whole settings file?  That would give me the mappings.
-		// Or, I could just take a bconf.  That would give me the plugins as well.
 
-		log("bconf: " + bconfPath);
-		
-		File bconfFile = getBconfFile(bconfPath);
-		// Should probably create a temp working space, since this needs a lot of file paths
+	public void setTgtLang(String value) {
+		this.tgtLang = value;
+	}
 
-		File baseDir = new File("/tmp/bconf"); // XXX
+	public void setSrcEncoding(String value) {
+		this.srcEncoding = value;
+	}
+
+	public void setTgtEncoding(String value) {
+		this.tgtEncoding = value;
+	}
+
+	public void setPipeline(String settingsPath) {
+		this.plnPath = settingsPath;
+	}
+
+	public void setFilterConfigDir(String filterConfigPath) {
+		this.filterConfigPath = filterConfigPath;
+	}
+
+	public void addFileset(FileSet fileset) {
+		this.filesets.add(fileset);
+	}
+
+	public FilterMapping createFilterMapping() {
+		FilterMapping fm = new FilterMapping();
+		filterMappings.add(fm);
+		return fm;
+	}
+
+	@Override
+	void checkConfiguration() throws BuildException {
+		TaskUtil.checkExists("srcLang", srcLang);
+		TaskUtil.checkExists("tgtLang", tgtLang);
+		TaskUtil.checkExists("srcEncoding", srcEncoding);
+		TaskUtil.checkExists("tgtEncoding", tgtEncoding);
+		TaskUtil.checkExists(plnPath, "plnPath");
+		if (filterConfigPath != null) {
+			TaskUtil.checkPath("filterConfigPath", filterConfigPath);
+		}
+		normalizeFilterMappings();
+	}
+
+	@Override
+	void executeWithOkapiClassloader() {
+		FilterConfigurationMapper fcMapper = getFilterMapper(filterConfigPath,
+				null);
 		PluginsManager plManager = new PluginsManager();
-		plManager.discover(new File(getConfigDirPath(baseDir)), true);
-		PipelineWrapper pipelineWrapper = preparePipelineWrapper(baseDir, plManager);
-        BatchConfiguration bconf = new BatchConfiguration();
-        bconf.installConfiguration(bconfFile.getAbsolutePath(),
-                getConfigDirPath(baseDir), pipelineWrapper);
+		PipelineWrapper pipelineWrapper = getPipelineWrapper(getProject()
+				.getBaseDir(), fcMapper, plManager);
+		pipelineWrapper.load(plnPath);
+		Project prj = createProject(pipelineWrapper);
+		for (FileSet fs : filesets) {
+			for (String s : fs.getDirectoryScanner().getIncludedFiles()) {
+				addFileToProject(prj, new File(s));
+			}
+		}
+		pipelineWrapper.execute(prj);
 	}
-	
-	// XXX This probably needs to expand the bconf somewhere temporary so that it can install 
-	// the plugins, etc?
-	private PipelineWrapper preparePipelineWrapper(File baseDir, PluginsManager plManager) {
-        // Load local plug-ins                                           
-		//plManager.discover(new File(WorkspaceUtils.getConfigDirPath(projId)), true);
-        
-		// Initialize filter configurations
-        FilterConfigurationMapper fcMapper = new FilterConfigurationMapper();
-        DefaultFilters.setMappings(fcMapper, false, true);
-        //fcMapper.addFromPlugins(plManager);
-        //fcMapper.setCustomConfigurationsDirectory(WorkspaceUtils.getConfigDirPath(projId));
-        fcMapper.updateCustomConfigurations();
 
-        // Load pipeline
-        /*
-        ExecutionContext context = new ExecutionContext();
-        context.setApplicationName("okapi-ant");
-        context.setIsNoPrompt(true);
-        PipelineWrapper pipelineWrapper = new PipelineWrapper(fcMapper, getConfigDirPath(baseDir),
-                plManager, getInputDirPath(baseDir), getInputDirPath(baseDir),
-                null, context);
-        pipelineWrapper.addFromPlugins(plManager);
-        return pipelineWrapper;
-        */
-        return null;
-    }
-	
-	private String getConfigDirPath(File base) {
-		return getPath(new File(base, "config"));
+	@Override
+	protected PipelineWrapper getPipelineWrapper(File baseDir,
+			FilterConfigurationMapper fcMapper, PluginsManager plManager) {
+		ExecutionContext context = new ExecutionContext();
+		context.setApplicationName("Longhorn");
+		context.setIsNoPrompt(true);
+		PipelineWrapper pipelineWrapper = new PipelineWrapper(fcMapper,
+				baseDir.getPath(), plManager, baseDir.getPath(),
+				baseDir.getPath(), baseDir.getPath(), null, context);
+		pipelineWrapper.addFromPlugins(plManager);
+		return pipelineWrapper;
 	}
-	
-	private String getInputDirPath(File base) {
-		return getPath(new File(base, "input"));
+
+	// Based on code from ProjectUtils in longhorn
+	private Project createProject(PipelineWrapper pipelineWrapper) {
+		Project project = new Project(new LanguageManager());
+		String projectPath = getProject().getBaseDir().getAbsolutePath();
+		project.setSourceLanguage(new LocaleId(srcLang, true));
+		project.setTargetLanguage(new LocaleId(tgtLang, true));
+		project.setInputRoot(0, projectPath, true);
+		project.setOutputRoot(projectPath);
+		project.setUseOutputRoot(true);
+		return project;
 	}
-	
-	private String getPath(File dir) {
-		dir.mkdirs();
-		return dir.getPath();
-	}
-	
-	private File getBconfFile(String path) {
-		File f = new File(path);
-		if (!f.exists() || !f.isFile()) {
-			throw new BuildException("Not a bconf file: " + path);
+
+	private void addFileToProject(Project project, File file) {
+		String extension = getExtension(file.getName());
+		String filterConfigId = getFilterConfigMapping(extension);
+		if (filterConfigId == null) {
+			throw new BuildException(
+					"Unable to find a filter configuration for "
+							+ file.getName());
 		}
-		return f;
-	}
-	
-	private void checkConfiguration() {
-		if (bconfPath == null) {
-			throw new BuildException(BCONF_ATTR + " was not set");
+		if (project.addDocument(0, file.getAbsolutePath(), null, null,
+				filterConfigId, false) == 1) {
+			throw new BuildException("Adding document " + file.getName()
+					+ " to list of input files failed");
 		}
+	}
+
+	private String getFilterConfigMapping(String fileExtension) {
+		for (FilterMapping fm : filterMappings) {
+			if (fm.extension.equals(fileExtension)) {
+				return fm.filterConfig;
+			}
+		}
+		return null;
+	}
+
+	private void normalizeFilterMappings() {
+		for (FilterMapping fm : filterMappings) {
+			if (!fm.extension.startsWith(".")) {
+				fm.extension = "." + fm.extension;
+			}
+		}
+	}
+
+	private String getExtension(String filename) {
+		int i = filename.lastIndexOf('.');
+		if (i == -1) {
+			return "";
+		}
+		return filename.substring(i);
 	}
 }
